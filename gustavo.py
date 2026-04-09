30379f5347277488e490282214ff8776276dcd0d
mh dev <matheushenrique22547@gmail.com>
2026-04-09 19:30:21 -0300

    estudo sobre mdulos

---
+++ gustavo.py
@@ -0,0 +1,12 @@
+import math 
+import os
+import modulo
+print(math.pow(5,5))
+print(math.ceil(4.1))
+print(math.ceil(4.8))
+print(math.ceil(3.2))
+print(math.trunc(3.2))
+print(modulo.soma(20 ,5))
+print(modulo.div(3 ,1))
+print(modulo.sub(5 ,3))
+print(modulo.mult(9 ,9))
